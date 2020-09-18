9c231d73c972c0a148912811e0a42f9e13696d50
Josh Barth <josh.barth@visiblescm.com>
2020-09-17 19:07:50 -0600

    initial commit

---
+++ personal/init.el
@@ -0,0 +1,18 @@
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(blink-cursor-mode -1)
+
+(setq inhibit-startup-screen t
+      initial-scratch-message "")
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(use-package base16-theme
+             :ensure t
+             :config
+             (load-theme 'base16-ashes t))
