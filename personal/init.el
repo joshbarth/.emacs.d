0c5c2ab4703560c8ff2beaf7f25811a96a9f52ab
Josh Barth <josh.barth@visiblescm.com>
2020-10-09 11:56:12 -0600

    changes on work machine

--- personal/init.el
+++ personal/init.el
@@ -3,6 +3,8 @@
 (scroll-bar-mode -1)
 (blink-cursor-mode -1)
 
+(setq whitespace-line-column 120)
+
 (setq inhibit-startup-screen t
       initial-scratch-message "")
 
@@ -15,4 +17,16 @@
 (use-package base16-theme
              :ensure t
              :config
-             (load-theme 'base16-ashes t))
+             (load-theme 'base16-atelier-forest t))
+
+(use-package fsharp-mode
+  :defer t
+  :ensure t)
+
+(require 'eglot-fsharp)
+
+(use-package restclient
+  :defer t
+  :ensure t)
+
+(add-hook 'fsharp-mode-hook 'rainbow-delimiters-mode)
